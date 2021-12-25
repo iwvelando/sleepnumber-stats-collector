594c9f843e1b57036fe68ef3fc16b24ad9d8ccef
Isaac Velando <isaac.velando@ibm.com>
2021-12-25 16:09:25 -0600

    use logrus for logging and use the Influx 2 client

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/iwvelando/sleepnumber-stats-collector
 go 1.13
 
 require (
-	github.com/influxdata/influxdb1-client v0.0.0-20200515024757-02f0bf5dbca3
+	github.com/influxdata/influxdb-client-go/v2 v2.6.0
 	github.com/iwvelando/SleepIQ v0.0.0-20190122071059-1531466e2b64
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/viper v1.7.0
-	go.uber.org/zap v1.15.0
 )
--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
-	_ "github.com/influxdata/influxdb1-client"
-	influxdb "github.com/influxdata/influxdb1-client"
+	influx "github.com/influxdata/influxdb-client-go/v2"
+	influxAPI "github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/iwvelando/SleepIQ"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"go.uber.org/zap"
-	"net/url"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 )
 
 // Configuration represents a YAML-formatted config file
 type Configuration struct {
-	SleepIQUsername       string
-	SleepIQPassword       string
-	PollInterval          time.Duration
-	InfluxDBURL           string
-	InfluxDBPort          int64
-	InfluxDBSkipVerifySsl bool
+	SleepIQUsername string
+	SleepIQPassword string
+	PollInterval    time.Duration
+	InfluxDB        InfluxDB
+}
+
+type InfluxDB struct {
+	Address           string
+	Username          string
+	Password          string
+	MeasurementPrefix string
+	Database          string
+	RetentionPolicy   string
+	Token             string
+	Organization      string
+	Bucket            string
+	SkipVerifySsl     bool
+	FlushInterval     uint
 }
 
 // Load a config file and return the Config struct
@@ -52,46 +66,58 @@ func BoolToInt(val bool) int8 {
 	return retVal
 }
 
-func main() {
+type InfluxWriteConfigError struct{}
 
-	// Initialize the structured logger
-	logger, err := zap.NewProduction()
-	if err != nil {
-		fmt.Println("{\"op\": \"main\", \"level\": \"fatal\", \"msg\": \"failed to initiate logger\"}")
-		os.Exit(1)
+func (r *InfluxWriteConfigError) Error() string {
+	return "must configure at least one of bucket or database/retention policy"
+}
+
+func InfluxConnect(config *Configuration) (influx.Client, influxAPI.WriteAPI, error) {
+	var auth string
+	if config.InfluxDB.Token != "" {
+		auth = config.InfluxDB.Token
+	} else if config.InfluxDB.Username != "" && config.InfluxDB.Password != "" {
+		auth = fmt.Sprintf("%s:%s", config.InfluxDB.Username, config.InfluxDB.Password)
+	} else {
+		auth = ""
 	}
-	defer logger.Sync()
+
+	var writeDest string
+	if config.InfluxDB.Bucket != "" {
+		writeDest = config.InfluxDB.Bucket
+	} else if config.InfluxDB.Database != "" && config.InfluxDB.RetentionPolicy != "" {
+		writeDest = fmt.Sprintf("%s/%s", config.InfluxDB.Database, config.InfluxDB.RetentionPolicy)
+	} else {
+		return nil, nil, &InfluxWriteConfigError{}
+	}
+
+	if config.InfluxDB.FlushInterval == 0 {
+		config.InfluxDB.FlushInterval = 30
+	}
+
+	options := influx.DefaultOptions().
+		SetFlushInterval(1000 * config.InfluxDB.FlushInterval).
+		SetTLSConfig(&tls.Config{
+			InsecureSkipVerify: config.InfluxDB.SkipVerifySsl,
+		})
+	client := influx.NewClientWithOptions(config.InfluxDB.Address, auth, options)
+
+	writeAPI := client.WriteAPI(config.InfluxDB.Organization, writeDest)
+
+	return client, writeAPI, nil
+}
+
+func main() {
 
 	// Load the config file based on path provided via CLI or the default
 	configLocation := flag.String("config", "config.yaml", "path to configuration file")
 	flag.Parse()
 	config, err := LoadConfiguration(*configLocation)
 	if err != nil {
-		logger.Fatal("failed to load configuration",
-			zap.String("op", "main"),
-			zap.Error(err),
-		)
-	}
-
-	// Initialize the InfluxDB connection
-	influxDBAddr := fmt.Sprintf("%s:%d", config.InfluxDBURL, config.InfluxDBPort)
-	host, err := url.Parse(influxDBAddr)
-	if err != nil {
-		logger.Fatal(fmt.Sprintf("failed to parse InfluxDB URL %s", influxDBAddr),
-			zap.String("op", "main"),
-			zap.Error(err),
-		)
-	}
-	influxConf := influxdb.Config{
-		URL:       *host,
-		UnsafeSsl: config.InfluxDBSkipVerifySsl,
-	}
-	influxConn, err := influxdb.NewClient(influxConf)
-	if err != nil {
-		logger.Fatal("failed to initialize InfluxDB connection",
-			zap.String("op", "main"),
-			zap.Error(err),
-		)
+		log.WithFields(log.Fields{
+			"op":    "main.LoadConfiguration",
+			"error": err,
+		}).Fatal("failed to load configuration")
 	}
 
 	// Initialize the SleepIQ client and login
@@ -99,196 +125,215 @@ func main() {
 
 	_, err = siq.Login(config.SleepIQUsername, config.SleepIQPassword)
 	if err != nil {
-		logger.Fatal("failed to log into SleepIQ account",
-			zap.String("op", "main"),
-			zap.Error(err),
-		)
+		log.WithFields(log.Fields{
+			"op":    "main",
+			"error": err,
+		}).Fatal("failed to log into SleepIQ account")
 	}
 
-	for {
+	// Initialize the InfluxDB connection
+	influxClient, writeAPI, err := InfluxConnect(config)
 
-		pollStartTime := time.Now()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"op":    "main",
+			"error": err,
+		}).Fatal("failed to initialize InfluxDB connection")
+	}
+	defer influxClient.Close()
+	defer writeAPI.Flush()
 
-		// Query all beds
-		beds, err := siq.Beds()
-		if err != nil {
-			logger.Error("failed to query beds",
-				zap.String("op", "main"),
-				zap.Error(err),
-			)
-			if strings.Contains(err.Error(), "Session is invalid") {
-				logger.Info("refreshing login due to invalid session",
-					zap.String("op", "main"),
-				)
-				_, err = siq.Login(config.SleepIQUsername, config.SleepIQPassword)
-				if err != nil {
-					logger.Fatal("failed to log into SleepIQ account",
-						zap.String("op", "main"),
-						zap.Error(err),
-					)
-				}
-			}
-			timeRemaining := config.PollInterval*time.Second - time.Since(pollStartTime)
-			time.Sleep(time.Duration(timeRemaining))
-			continue
+	errorsCh := writeAPI.Errors()
+
+	// Monitor InfluxDB write errors
+	go func() {
+		for err := range errorsCh {
+			log.WithFields(log.Fields{
+				"op":    "influxdb.WriteAll",
+				"error": err,
+			}).Error("encountered error on writing to InfluxDB")
 		}
+	}()
 
-		// Query all beds via family status
-		familyStatusBeds, err := siq.BedFamilyStatus()
-		tsFamilyStatus := time.Now()
-		if err != nil {
-			logger.Error("failed to query family status beds",
-				zap.String("op", "main"),
-				zap.Error(err),
-			)
-			if strings.Contains(err.Error(), "Session is invalid") {
-				logger.Info("refreshing login due to invalid session",
-					zap.String("op", "main"),
-				)
-				_, err = siq.Login(config.SleepIQUsername, config.SleepIQPassword)
-				if err != nil {
-					logger.Fatal("failed to log into SleepIQ account",
-						zap.String("op", "main"),
-						zap.Error(err),
-					)
-				}
-			}
-			timeRemaining := config.PollInterval*time.Second - time.Since(pollStartTime)
-			time.Sleep(time.Duration(timeRemaining))
-			continue
-		}
+	// Look for SIGTERM or SIGINT
+	cancelCh := make(chan os.Signal, 1)
+	signal.Notify(cancelCh, syscall.SIGTERM, syscall.SIGINT)
 
-		// Form the output data
-		data := make([]influxdb.Point, 0)
+	go func() {
+		for {
 
-		for _, bed := range beds.Beds {
+			pollStartTime := time.Now()
 
-			foundation, err := siq.BedFoundationStatus(bed.BedID)
+			// Query all beds
+			beds, err := siq.Beds()
 			if err != nil {
-				logger.Error("failed to query bed foundation status",
-					zap.String("op", "main"),
-					zap.Error(err),
-				)
+				log.WithFields(log.Fields{
+					"op":    "main",
+					"error": err,
+				}).Error("failed to query beds")
 				if strings.Contains(err.Error(), "Session is invalid") {
-					logger.Info("refreshing login due to invalid session",
-						zap.String("op", "main"),
-					)
+					log.WithFields(log.Fields{
+						"op": "main",
+					}).Info("refreshing login due to invalid session")
 					_, err = siq.Login(config.SleepIQUsername, config.SleepIQPassword)
 					if err != nil {
-						logger.Fatal("failed to log into SleepIQ account",
-							zap.String("op", "main"),
-							zap.Error(err),
-						)
+						log.WithFields(log.Fields{
+							"op":    "main",
+							"error": err,
+						}).Fatal("failed to log into SleepIQ account")
 					}
 				}
 				timeRemaining := config.PollInterval*time.Second - time.Since(pollStartTime)
 				time.Sleep(time.Duration(timeRemaining))
 				continue
 			}
-			tsFoundation := time.Now()
-			data = append(data, influxdb.Point{
-				Measurement: "bed_foundation_state",
-				Tags: map[string]string{
-					"size":       bed.Size,
-					"name":       bed.Name,
-					"generation": bed.Generation,
-					"model":      bed.Model,
-					"type":       foundation.Type,
-				},
-				Fields: map[string]interface{}{
-					"is_moving":                     BoolToInt(foundation.IsMoving),
-					"current_position_preset_right": foundation.CurrentPositionPresetRight,
-					"current_position_preset_left":  foundation.CurrentPositionPresetLeft,
-					"right_head_position":           foundation.RightHeadPosition,
-					"left_head_position":            foundation.LeftHeadPosition,
-					"right_foot_position":           foundation.RightFootPosition,
-					"left_foot_position":            foundation.LeftFootPosition,
-				},
-				Time:      tsFoundation,
-				Precision: "ns",
-			})
 
-			footwarmers, err := siq.BedFootWarmerStatus(bed.BedID)
+			// Query all beds via family status
+			familyStatusBeds, err := siq.BedFamilyStatus()
+			tsFamilyStatus := time.Now()
 			if err != nil {
-				logger.Error("failed to query bed foundation status",
-					zap.String("op", "main"),
-					zap.Error(err),
-				)
+				log.WithFields(log.Fields{
+					"op":    "main",
+					"error": err,
+				}).Error("failed to query family status beds")
 				if strings.Contains(err.Error(), "Session is invalid") {
-					logger.Info("refreshing login due to invalid session",
-						zap.String("op", "main"),
-					)
+					log.WithFields(log.Fields{
+						"op":    "main",
+						"error": err,
+					}).Info("refreshing login due to invalid session")
 					_, err = siq.Login(config.SleepIQUsername, config.SleepIQPassword)
 					if err != nil {
-						logger.Fatal("failed to log into SleepIQ account",
-							zap.String("op", "main"),
-							zap.Error(err),
-						)
+						log.WithFields(log.Fields{
+							"op":    "main",
+							"error": err,
+						}).Fatal("failed to log into SleepIQ account")
 					}
 				}
 				timeRemaining := config.PollInterval*time.Second - time.Since(pollStartTime)
 				time.Sleep(time.Duration(timeRemaining))
 				continue
 			}
-			tsFootwarmers := time.Now()
-			data = append(data, influxdb.Point{
-				Measurement: "bed_footwarmers_state",
-				Tags: map[string]string{
-					"size":       bed.Size,
-					"name":       bed.Name,
-					"generation": bed.Generation,
-					"model":      bed.Model,
-				},
-				Fields: map[string]interface{}{
-					"foot_warming_status_left":  footwarmers.FootWarmingStatusLeft,
-					"foot_warming_status_right": footwarmers.FootWarmingStatusRight,
-				},
-				Time:      tsFootwarmers,
-				Precision: "ns",
-			})
 
-			for _, familyStatusBed := range familyStatusBeds.Beds {
-				if familyStatusBed.BedID == bed.BedID {
-					data = append(data, influxdb.Point{
-						Measurement: "bed_sleeper_state",
-						Tags: map[string]string{
-							"size":       bed.Size,
-							"name":       bed.Name,
-							"generation": bed.Generation,
-							"model":      bed.Model,
-						},
-						Fields: map[string]interface{}{
-							"left_sleeper_is_in_bed":  BoolToInt(familyStatusBed.LeftSide.IsInBed),
-							"right_sleeper_is_in_bed": BoolToInt(familyStatusBed.RightSide.IsInBed),
-							"left_sleep_number":       familyStatusBed.LeftSide.SleepNumber,
-							"right_sleep_number":      familyStatusBed.RightSide.SleepNumber,
-							"left_pressure":           familyStatusBed.LeftSide.Pressure,
-							"right_pressure":          familyStatusBed.RightSide.Pressure,
-						},
-						Time:      tsFamilyStatus,
-						Precision: "ns",
-					})
+			for _, bed := range beds.Beds {
+
+				foundation, err := siq.BedFoundationStatus(bed.BedID)
+				if err != nil {
+					log.WithFields(log.Fields{
+						"op":    "main",
+						"error": err,
+					}).Error("failed to query bed foundation status")
+					if strings.Contains(err.Error(), "Session is invalid") {
+						log.WithFields(log.Fields{
+							"op": "main",
+						}).Info("refreshing login due to invalid session")
+						_, err = siq.Login(config.SleepIQUsername, config.SleepIQPassword)
+						if err != nil {
+							log.WithFields(log.Fields{
+								"op":    "main",
+								"error": err,
+							}).Fatal("failed to log into SleepIQ account")
+						}
+					}
+					timeRemaining := config.PollInterval*time.Second - time.Since(pollStartTime)
+					time.Sleep(time.Duration(timeRemaining))
+					continue
+				}
+				tsFoundation := time.Now()
+				data := influx.NewPoint(
+					"bed_foundation_state",
+					map[string]string{
+						"size":       bed.Size,
+						"name":       bed.Name,
+						"generation": bed.Generation,
+						"model":      bed.Model,
+						"type":       foundation.Type,
+					},
+					map[string]interface{}{
+						"is_moving":                     BoolToInt(foundation.IsMoving),
+						"current_position_preset_right": foundation.CurrentPositionPresetRight,
+						"current_position_preset_left":  foundation.CurrentPositionPresetLeft,
+						"right_head_position":           foundation.RightHeadPosition,
+						"left_head_position":            foundation.LeftHeadPosition,
+						"right_foot_position":           foundation.RightFootPosition,
+						"left_foot_position":            foundation.LeftFootPosition,
+					},
+					tsFoundation,
+				)
+				writeAPI.WritePoint(data)
+
+				footwarmers, err := siq.BedFootWarmerStatus(bed.BedID)
+				if err != nil {
+					log.WithFields(log.Fields{
+						"op":    "main",
+						"error": err,
+					}).Error("failed to query bed foundation status")
+					if strings.Contains(err.Error(), "Session is invalid") {
+						log.WithFields(log.Fields{
+							"op": "main",
+						}).Info("refreshing login due to invalid session")
+						_, err = siq.Login(config.SleepIQUsername, config.SleepIQPassword)
+						if err != nil {
+							log.WithFields(log.Fields{
+								"op":    "main",
+								"error": err,
+							}).Fatal("failed to log into SleepIQ account")
+						}
+					}
+					timeRemaining := config.PollInterval*time.Second - time.Since(pollStartTime)
+					time.Sleep(time.Duration(timeRemaining))
+					continue
+				}
+				tsFootwarmers := time.Now()
+				data = influx.NewPoint(
+					"bed_footwarmers_state",
+					map[string]string{
+						"size":       bed.Size,
+						"name":       bed.Name,
+						"generation": bed.Generation,
+						"model":      bed.Model,
+					},
+					map[string]interface{}{
+						"foot_warming_status_left":  footwarmers.FootWarmingStatusLeft,
+						"foot_warming_status_right": footwarmers.FootWarmingStatusRight,
+					},
+					tsFootwarmers,
+				)
+				writeAPI.WritePoint(data)
+
+				for _, familyStatusBed := range familyStatusBeds.Beds {
+					if familyStatusBed.BedID == bed.BedID {
+						data := influx.NewPoint(
+							"bed_sleeper_state",
+							map[string]string{
+								"size":       bed.Size,
+								"name":       bed.Name,
+								"generation": bed.Generation,
+								"model":      bed.Model,
+							},
+							map[string]interface{}{
+								"left_sleeper_is_in_bed":  BoolToInt(familyStatusBed.LeftSide.IsInBed),
+								"right_sleeper_is_in_bed": BoolToInt(familyStatusBed.RightSide.IsInBed),
+								"left_sleep_number":       familyStatusBed.LeftSide.SleepNumber,
+								"right_sleep_number":      familyStatusBed.RightSide.SleepNumber,
+								"left_pressure":           familyStatusBed.LeftSide.Pressure,
+								"right_pressure":          familyStatusBed.RightSide.Pressure,
+							},
+							tsFamilyStatus,
+						)
+						writeAPI.WritePoint(data)
+					}
 				}
 			}
+
+			timeRemaining := config.PollInterval*time.Second - time.Since(pollStartTime)
+			time.Sleep(time.Duration(timeRemaining))
+
 		}
+	}()
 
-		batchData := influxdb.BatchPoints{
-			Points:          data,
-			Database:        "sleepnumber",
-			RetentionPolicy: "autogen",
-		}
-
-		_, err = influxConn.Write(batchData)
-		if err != nil {
-			logger.Warn("failed to write to InfluxDB",
-				zap.String("op", "main"),
-				zap.Error(err),
-			)
-		}
-
-		timeRemaining := config.PollInterval*time.Second - time.Since(pollStartTime)
-		time.Sleep(time.Duration(timeRemaining))
-
-	}
-
+	sig := <-cancelCh
+	log.WithFields(log.Fields{
+		"op": "main",
+	}).Info(fmt.Sprintf("caught signal %v, flushing data to InfluxDB", sig))
+	writeAPI.Flush()
 }
